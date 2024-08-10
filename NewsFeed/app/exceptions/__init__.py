df6146d41ab1f9198657653bf56b6cd76ddcb6ec
AshourDono <eng.ahmed.hamdy.ashour@gmail.com>
2024-08-10 14:02:42 +0300

    Edited db, added error handling to controller methods, and make db connection a singleton class.
        [Attachments/newsfeed.sql]:
            - Added 'on delete cascade' to all foreign keys.
    
        [NewsFeed/app/controllers/post_controller.py]:
            - Imported exception custom interfaces.
            - Added input validation check.
            - Added error handling.
    
        [NewsFeed/app/exceptions]:
            - Added new file: exceptions.py.
            - Updated imports in __init__.py.
    
        [NewsFeed/app/extensions/database.py]:
            - Converted get_db_connection to singleton class.

---
+++ NewsFeed/app/exceptions/__init__.py
@@ -0,0 +1,1 @@
+from .exceptions import InvalidPostDataException, PostNotFoundException
---
+++ NewsFeed/app/exceptions/exceptions.py
@@ -0,0 +1,7 @@
+# app/exceptions/exceptions.py
+class PostNotFoundException(Exception):
+    pass
+
+
+class InvalidPostDataException(Exception):
+    pass
